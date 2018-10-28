ba430b4bfe905b554dbf7f4280c4fb89e61a0e6c
Purnell Jones <plj.coding@gmail.com>
2018-10-27 23:00:24 -0400

    Updated Workout Details Images

--- js/muscleFactory.js
+++ js/muscleFactory.js
@@ -11,28 +11,28 @@ app.factory('MuscleFactory', function() {
                 "body" : "This is chest rule of thumb",
             }
         },
-        "bicep" : { "muscles" :[ { "name" : "Biceps Brachii (Short Head)","path" : "./img/chest-clav.jpeg" }, { "name" :"Biceps Brachii (Long Head)","path" : "./img/chest-clav.jpeg" }, { "name" : "Brachialis","path" : "./img/chest-clav.jpeg" }],
+        "bicep" : { "muscles" :[ { "name" : "Biceps Brachii (Short Head)","path" : "./img/biceps.jpg" }, { "name" :"Biceps Brachii (Long Head)","path" : "./img/biceps.jpg" }, { "name" : "Brachialis","path" : "./img/biceps.jpg" }],
         "details" : {
             "group" : "bicep",    
             "title" : "",
                 "body" : "This is bicep rule of thumb",
             }
         },
-        "tricep" : { "muscles" :[{"name" : "Long Head", "path" : "./img/chest-clav.jpeg"}, {"name" : "Lateral head","path" : "./img/chest-clav.jpeg"}, {"name" : "Medial head","path" : "./img/chest-clav.jpeg"}],
+        "tricep" : { "muscles" :[{"name" : "Long Head", "path" : "./img/triceps.jpeg"}, {"name" : "Lateral head","path" : "./img/triceps.jpeg"}, {"name" : "Medial head","path" : "./img/triceps.jpeg"}],
         "details" : {
             "group" : "tricep",    
             "title" : "",
                 "body" : "This is tricep rule of thumb",
             }
         },
-        "forearm" : { "muscles" :[{}],
+        "forearm" : { "muscles" :[{ "name" : "brachioradialis", "path" : "./img/forearm.jpg" }, { "name" : "flexors", "path" : "./img/forearm.jpg" }, { "name" : "extensors", "path" : "./img/forearm.jpg"} ],
         "details" : {
             "group" : "foream",    
             "title" : "",
                 "body" : "This is foream rule of thumb",
             }
         },
-        "abdominal" : { "muscles" :[ { "name" : "External Obliques", "path" : "./img/chest-clav.jpeg"}, { "name" : "Internal Obliques", "path" : "./img/chest-clav.jpeg"}, { "name" : "Rectus Abdominis", "path" : "./img/chest-clav.jpeg"}],
+        "abdominal" : { "muscles" :[ { "name" : "External Obliques", "path" : "./img/abdominals.jpg"}, { "name" : "Internal Obliques", "path" : "./img/abdominals.jpeg"}, { "name" : "Rectus Abdominis", "path" : "./img/abdominals.jpeg"}],
         "details" : {
             "group" : "abdominal",    
             "title" : "",
@@ -46,28 +46,28 @@ app.factory('MuscleFactory', function() {
                 "body" : "This is shoulders rule of thumb",
             }
         },
-        "calf" : { "muscles" :[{ "name" : "Gastrocnemius medial", "path" : "./img/chest-clav.jpeg"}, {"name" : "Gastrocnemius lateral", "path" : "./img/chest-clav.jpeg"}, {"name" : "Soleus", "path" : "./img/chest-clav.jpeg"}],
+        "calf" : { "muscles" :[{ "name" : "Gastrocnemius medial", "path" : "./img/calves.jpg"}, {"name" : "Gastrocnemius lateral", "path" : "./img/calves.jpg"}, {"name" : "Soleus", "path" : "./img/calves.jpg"}],
         "details" : {
             "group" : "calf",    
             "title" : "",
                 "body" : "This is calves rule of thumb rule of thumb",
             }
         },
-        "back" : { "muscles" :[ {"name" : "Upper Trapezius", "path" : "./img/chest-clav.jpeg" }, {"name" : "Middle Trapezius", "path" : "./img/chest-clav.jpeg" } , {"name" : "Lower Trapezius", "path" : "./img/chest-clav.jpeg" },  {"name" : "Latissimus Dorsi", "path" : "./img/chest-clav.jpeg" }, {"name" : "Teres minor", "path" : "./img/chest-clav.jpeg" }, {"name" : "Teres Major", "path" : "./img/chest-clav.jpeg" }, {"name" : "Infraspinatus", "path" : "./img/chest-clav.jpeg" }, {"name" : "Super Spanaius", "path" : "./img/chest-clav.jpeg" }, {"name" : "Infer Spanaius", "path" : "./img/chest-clav.jpeg" }, {"name" : "Rhomboid", "path" : "./img/chest-clav.jpeg" }, {"name" : "Lower Back", "path" : "./img/chest-clav.jpeg" }],
+        "back" : { "muscles" :[ {"name" : "Upper Trapezius", "path" : "./img/back.jpg" }, {"name" : "Middle Trapezius", "path" : "./img/back.jpg" } , {"name" : "Lower Trapezius", "path" : "./img/back.jpg" },  {"name" : "Latissimus Dorsi", "path" : "./img/back.jpg" }, {"name" : "Teres minor", "path" : "./img/back.jpg" }, {"name" : "Teres Major", "path" : "./img/back.jpg" }, {"name" : "Infraspinatus", "path" : "./img/back.jpg" }, {"name" : "Super Spanaius", "path" : "./img/back.jpg" }, {"name" : "Infer Spanaius", "path" : "./img/back.jpg" }, {"name" : "Rhomboid", "path" : "./img/back.jpg" }, {"name" : "Lower Back", "path" : "./img/back.jpg" }],
         "details" : {
             "group" : "back",    
             "title" : "",
                 "body" : "This is back rule of thumb rule of thumb",
             }
         },
-        "hamstring" : { "muscles" :[{ "name" : "Semitendinosus", "path" : "./img/chest-clav.jpeg"}, { "name" : "Semimembranosus", "path" : "./img/chest-clav.jpeg"}, {"name" : "Biceps femoris", "path" : "./img/chest-clav.jpeg"}],
+        "hamstring" : { "muscles" :[{ "name" : "Semitendinosus", "path" : "./img/hamstrings.jpg"}, { "name" : "Semimembranosus", "path" : "./img/hamstrings.jpg"}, {"name" : "Biceps femoris", "path" : "./img/hamstrings.jpg"}],
         "details" : {
             "group" : "hamstring",    
             "title" : "",
                 "body" : "This is hamstring rule of thumb rule of thumb",
             }
         },
-        "quad" : { "muscles" :[{ "name" : "Rectus Femoris", "path" : "./img/chest-clav.jpeg"}, {"name" : "Vastus Lateralis", "path" : "./img/chest-clav.jpeg"}, {"name" : "Vastus Intermedius", "path" : "./img/chest-clav.jpeg"}, {"name" : "Vastus Medialis", "path" : "./img/chest-clav.jpeg"}],
+        "quad" : { "muscles" :[{ "name" : "Rectus Femoris", "path" : "./img/quads.jpg"}, {"name" : "Vastus Lateralis", "path" : "./img/quads.jpg"}, {"name" : "Vastus Intermedius", "path" : "./img/quads.jpg"}, {"name" : "Vastus Medialis", "path" : "./img/quads.jpg"}],
         "details" : {
             "group" : "quad",    
             "title" : "",
@@ -75,7 +75,7 @@ app.factory('MuscleFactory', function() {
             }
         },
         "glutes" : {
-            "muscles" : [{ "name" : "Gluteus Maximus", "path" : "./img/chest-clav.jpeg"}, { "name" : "Gluteus Medius", "path" : "./img/chest-clav.jpeg"}],
+            "muscles" : [{ "name" : "Gluteus Maximus", "path" : "./img/glutes.jpg"}, { "name" : "Gluteus Medius", "path" : "./img/glutes.jpg"}],
             "details" : {
                 "group" : "glutes",
                 "title" : "This is the modal title for glutes",
